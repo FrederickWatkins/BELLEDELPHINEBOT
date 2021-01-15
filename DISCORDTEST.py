d6ae46ed3de3bc429f700316c901fc4bc22df51b
Upsilon1 <31772157+Upsilon1@users.noreply.github.com>
2021-01-15 19:52:32 +0000

    First commit

---
+++ DISCORDTEST.py
@@ -0,0 +1,31 @@
+import os
+import random
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    await client.wait_until_ready()
+    if message.author == client.user:
+        return
+
+    if "💦" in message.content:
+        print("Splash found in message")
+        for x in message.content:
+            if x == "💦":
+                await message.channel.send(file=discord.File("Images/"+random.choice(os.listdir('Images/'))))
+    if "🍆" in message.content:
+        print("Eggplant detected")
+        await message.channel.send("👀")
+
+client.run(TOKEN)
